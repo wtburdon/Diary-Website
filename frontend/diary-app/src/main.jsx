14e5908b0d4f699d7690ddb0123fc929bdfca312
wtburdon <billyburdon@outlook.com>
2024-11-14 17:23:15 -0700

    added navbar componenet

--- frontend/diary-app/src/main.jsx
+++ frontend/diary-app/src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Navbar from './navbar.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Navbar/>
   </StrictMode>,
 )
