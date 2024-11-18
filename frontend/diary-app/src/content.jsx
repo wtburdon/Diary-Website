e3f8572c6d37b75768d594dda597e20c333d4d71
wtburdon <billyburdon@outlook.com>
2024-11-18 10:39:31 -0700

    designing content page

---
+++ frontend/diary-app/src/content.jsx
@@ -0,0 +1,14 @@
+
+function contentPage(){
+    return(
+        <div>
+            <div class="border"></div>
+            <div>
+                <p></p>
+            </div>
+            <div class="border"></div>
+        </div>
+    )
+}
+
+export default contentPage
--- frontend/diary-app/src/main.jsx
+++ frontend/diary-app/src/main.jsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import Navbar from './navbar.jsx'
+import Content from './content.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Navbar/>
+    <Content/>
   </StrictMode>,
 )
