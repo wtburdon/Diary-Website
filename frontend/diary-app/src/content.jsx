57dddaa886c390eb494c30c92a53a3b7a99078e2
wtburdon <billyburdon@outlook.com>
2024-11-18 11:09:09 -0700

    changed class to className

--- frontend/diary-app/src/content.jsx
+++ frontend/diary-app/src/content.jsx
@@ -3,11 +3,11 @@ function contentPage(){
     return(
         <section>
             <div>
-                <div class="border"></div>
+                <div className="border"></div>
                 <div>
                 <p>CONTENT</p>
                 </div>
-                <div class="border"></div>
+                <div className="border"></div>
             </div>
         </section>
     )
--- frontend/diary-app/src/navbar.jsx
+++ frontend/diary-app/src/navbar.jsx
@@ -3,7 +3,7 @@ import './navbar.css'
 function navbar(){
     return(
         <section>
-            <div class="navbar-div">
+            <div className="navbar-div">
                 <button>About</button>
                 <button>Contact Me</button>
                 <button>Login</button>
