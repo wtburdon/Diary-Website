91d39ee8589a26f250122e6d3c9940934eb6e94c
wtburdon <billyburdon@outlook.com>
2024-11-18 11:43:38 -0700

    setting up entry component functions

--- frontend/diary-app/src/content.jsx
+++ frontend/diary-app/src/content.jsx
@@ -1,16 +1,34 @@
 
+function Entry(props){
+    return <li>{props.entry}</li>
+}
+
+
+function EntryList(props){
+    return(
+        <ul>
+            {props.entries.map( (entry) => {
+                return <Entry key={entry} entry={entry} />
+            })}
+        </ul>
+    );
+}
+
 function contentPage(){
+    const Entries = ['Nov 11', 'Oct 31', 'Oct 24']
+    
     return(
         <section>
             <div>
                 <div className="border"></div>
                 <div>
-                <p>CONTENT</p>
+                <h1>CONTENT</h1>
+                <EntryList entries={Entries}/>
                 </div>
                 <div className="border"></div>
             </div>
         </section>
-    )
+    );
 }
 
 export default contentPage
