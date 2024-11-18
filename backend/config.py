ff8b546aaeae7a8461576a40c350116e26daa181
wtburdon <billyburdon@outlook.com>
2024-11-18 12:56:03 -0700

    added JWT and bcrypt to config for user authentication

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 CORS(app)
@@ -9,4 +11,5 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False #turn off later
 
 db = SQLAlchemy(app)
-
+bcrypt = Bcrypt(app) #bcrypt for hashing passwords
+jwt = JWTManager(app) #Json Web Token is for verifying user authentication
